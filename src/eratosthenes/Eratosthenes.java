dce548351b6f55277357daf68bc6f6fca47435a2
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-03-01 10:51:10 -0500

    Update Eratosthenes.java

--- src/eratosthenes/Eratosthenes.java
+++ src/eratosthenes/Eratosthenes.java
@@ -1,34 +1,36 @@
 /*
     Matthew Ludwig
     28 February 2019
-    This program displays a list of prime numbers in between 1 and 1000
+    This program displays a list of prime numbers in between 2 and another integer
 */
-package erastothenes;
+package eratosthenes;
+import java.util.Scanner; 
+import java.util.Arrays;
 
 /**
  *
  * @author malud0519
  */
-public class Erastothenes {
+public class Eratosthenes {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        boolean numbers [] = new boolean[1000]; // prime numbers denoted as true
-        // sets each value to be true
-        System.out.println("The prime numbers in between 1 and 1000 are: ");
-        for (int i = 1; i < numbers.length; i++) {
-           numbers[i] = true;
-        }  
-       
+        Scanner inputNum = new Scanner(System.in);
+        System.out.println("Enter the largest number: ");
+        int range = Integer.parseInt(inputNum.nextLine());
+        boolean numbers [] = new boolean[range]; // prime numbers denoted as true
+        System.out.println("The prime numbers in between 2 and " + range );
+        Arrays.fill(numbers,true); // default state
+        
         for (int j = 2;  j<= numbers.length; j++) {
             // if the number is prime
             if (numbers[j-1] == true) {
                 System.out.println(j);
                 // if the number at each index is not prime
-                for (int k = (int)Math.pow(j, 2); k <= numbers.length; k +=j) {
-                    numbers[k-1] = false;
+                for (int k = (int)Math.pow(j, 2); k <= numbers.length; k +=j) {  // every square of j is eliminated
+                    numbers[k-1] = false; // each index that isn't prime is eliminated 
                 }
             }
         }
