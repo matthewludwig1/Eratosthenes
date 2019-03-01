af9621375f767635170ee77a94ef72fc76b41502
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-02-28 23:52:35 -0500

    Update Eratosthenes.java

--- src/eratosthenes/Eratosthenes.java
+++ src/eratosthenes/Eratosthenes.java
@@ -1,31 +1,36 @@
 /*
     Matthew Ludwig
-    28 February
- */
-
-package eratosthenes;
+    28 February 2019
+    This program displays a list of prime numbers in between 1 and 1000
+*/
+package erastothenes;
 
 /**
  *
  * @author malud0519
  */
-public class Eratosthenes {
+public class Erastothenes {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        boolean numbers [] = new boolean[1000];
-       
+        boolean numbers [] = new boolean[1000]; // prime numbers denoted as true
+        // sets each value to be true
+        System.out.println("The prime numbers in between 1 and 1000 are: ");
         for (int i = 1; i < numbers.length; i++) {
            numbers[i] = true;
-           for (int j = i + i + 1; j < numbers.length; j++) {
-               if (j % i + 1  == 0) {
-                  numbers[i] = false;
-               }
-           }
- // could use a while statements
-          System.out.println(i + 1);
-      }  
+        }  
+       
+        for (int j = 2;  j<= numbers.length; j++) {
+            // if the number is prime
+            if (numbers[j-1] == true) {
+                System.out.println(j);
+                // if the number at each index is not prime
+                for (int k = j * j; k <= numbers.length; k +=j) {
+                    numbers[k-1] = false;
+                }
+            }
+        }
     }
 }
